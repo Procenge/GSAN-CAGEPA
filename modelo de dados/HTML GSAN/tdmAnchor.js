e27962beee1889804ee3d84ff2dad9b074a794da
Procenge <Yara.Souza@procenge.com.br>
2014-10-17 17:50:31 -0300

    Modelo-Dados

---
+++ modelo de dados/HTML GSAN/tdmAnchor.js
@@ -0,0 +1,15 @@
+﻿function getUrlForFrame()
+{
+var myItem, myPosition, newUrl, myExtension, myObjectLink;
+myExtension = window.location.href.split(".").pop(); 
+myFileExtension = myExtension.substr(0, myExtension.indexOf("#"));
+myPosition = window.location.href.lastIndexOf(".");
+myItem = window.location.href.substr(0, myPosition);
+myObjectLink = window.location.href.substr(window.location.href.indexOf("#"));
+if (window.location.href.lastIndexOf("#") != -1)
+  {	
+	  newUrl = myItem+"B."+myFileExtension+myObjectLink;
+	  window.frames['body'].location.href = newUrl;
+  }
+} 
+
